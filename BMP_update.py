98fb543dbf3adf57b26997e0ea0ab0ffd06c7b0f
Brent Freeman <freeman.brent.w@gmail.com>
2020-05-07 14:03:02 -0700

    Final test of Github desktop
    added one line

--- BMP_update.py
+++ BMP_update.py
@@ -13,6 +13,8 @@ from utilities import reorder_fields, rename_fields, addMessage
 from BMP_tools import add_StandardFields, incrementField, fillField, fillField_fromAnother,fillField_ifOverlap, fillField_Conditional,calcField_fromOverlap,fillField_fromDict,calcField_withinDistance
 
 #Testing Github Desktop
+# the test was successful
+
 arcpy.env.overwriteOutput = True
 
 temp = r"C:\temp\BMP_working.gdb"
